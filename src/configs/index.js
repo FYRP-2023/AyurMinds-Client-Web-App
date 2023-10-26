5698317b9d7551c092c99333d798f6a7dde2a368
amila1998 <amiladevin@gmail.com>
2023-10-26 23:00:46 +0530

    dd

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,6 +1,6 @@
 export const configs = {
-  API_GATWAY_URL: "http://localhost:5000",
-  // API_GATWAY_URL: "http://ayurminds-gateway.azurewebsites.net",
+  // API_GATWAY_URL: "http://localhost:5000",
+  API_GATWAY_URL: "https://ayurminds-gateway.azurewebsites.net",
   AUTHENTICATION_SERVICE: "/api/authentication_service",
   AUTHERIZATION_SERVICE: "/api/authorization_service",
   CHAT_SERVICE: "/api/chat_service",
